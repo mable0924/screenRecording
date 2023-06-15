7efed1a21a288f4c41c87374f639da47b06ad4ca
Mable0924 <pohinma@yahoo.com.hk>
2023-06-16 01:52:15 +0800

    Initial Commit

---
+++ screenRecording/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  screenRecording
+//
+//  Created by Po hin Ma on 16/6/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ screenRecording/screenRecordingApp.swift
@@ -0,0 +1,17 @@
+//
+//  screenRecordingApp.swift
+//  screenRecording
+//
+//  Created by Po hin Ma on 16/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct screenRecordingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
